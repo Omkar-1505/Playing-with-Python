92197cab8afc85922569d8d918d6accfd95eba81
Omkar-1505 <omkarpadheetheboss@gmail.com>
2026-05-18 22:46:19 +0530

    Bubble sort

---
+++ Bubble_sort.py
@@ -0,0 +1,12 @@
+#O(n^2),goes by each swapping
+def Bubblesort(a):
+    n = len(a)
+    for i in range(n):
+       for j in range(0,n-1-i): 
+           if(a[j] > a[j+1]):
+               a[j],a[j+1]=a[j+1],a[j]
+        
+a = [64,32,25,45,40,51,2]
+print(a)
+Bubblesort(a)
+print(a)
